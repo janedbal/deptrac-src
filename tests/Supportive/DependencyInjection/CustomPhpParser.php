ac7b421d95d812669dc5ab19b5c671639f18eb86
Jan Nedbal <janedbal@gmail.com>
2024-11-15 13:37:15 +0100

    ServiceContainerBuilderTest: test service override

---
+++ tests/Supportive/DependencyInjection/CustomPhpParser.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tests\Qossmic\Deptrac\Supportive\DependencyInjection;
+
+use Qossmic\Deptrac\Core\Ast\Parser\NikicPhpParser\NikicPhpParser;
+
+class CustomPhpParser extends NikicPhpParser
+{
+
+}
